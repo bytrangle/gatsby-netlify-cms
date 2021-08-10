860565aed09cd7539227556d9084b0dfdec62e65
Trang Le <lethutrang101@gmail.com>
2021-08-10 16:33:50 +0700

    Create markdown pages programmatically

--- gatsby-node.js
+++ gatsby-node.js
@@ -3,5 +3,36 @@
  *
  * See: https://www.gatsbyjs.com/docs/node-apis/
  */
+const path = require("path")
+const { createFilePath } = require(`gatsby-source-filesystem`)
 
-// You can delete this file if you're not using it
+// exports.onCreateNode = ({ node, getNode, actions }) => {
+//   const { createNodeField } = actions
+
+// }
+
+exports.createPages = async ({ graphql, actions }) => {
+  const { createPage } = actions
+  const result = await graphql(`
+    query {
+      allMarkdownRemark {
+        edges {
+          node {
+            frontmatter {
+              path
+            }
+          }
+        }
+      }
+    }
+  `)
+  result.data.allMarkdownRemark.edges.forEach(({ node }) => {
+    createPage({
+      path: `/${node.frontmatter.path}`,
+      component: path.resolve(`./src/templates/blog-post.js`),
+      context: {
+        slug: node.frontmatter.path
+      }
+    })
+  })
+}
