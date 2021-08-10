65d0dbda12be3e666753adb4fe6748e74e290b4c
Trang Le <lethutrang101@gmail.com>
2021-08-10 16:50:04 +0700

    Pull in data from Markdown files

--- src/templates/blog-post.js
+++ src/templates/blog-post.js
@@ -1,10 +1,26 @@
 import React from "react"
+import { graphql } from "gatsby"
 import Layout from "../components/layout"
 
-export default function Blogpost() {
+export default function Blogpost({ data }) {
+  const post = data.markdownRemark
   return (
     <Layout>
-      <div>Hello blog post</div>
+      <div>
+        <h1>{post.frontmatter.title}</h1>
+        <div dangerouslySetInnerHTML={{__html: post.html}}/>
+      </div>
     </Layout>
   )
 }
+
+export const query = graphql`
+  query($slug: String!) {
+    markdownRemark(frontmatter: {path: {eq: "insert-image-from-cloudinary"}}) {
+      html
+      frontmatter {
+        title
+      }
+    }
+  }
+`
