850626d384bf8ee0c344e0c2527da6109f547601
Trang Le <lethutrang101@gmail.com>
2021-08-10 17:21:53 +0700

    Remove cpage creation function

--- gatsby-node.js
+++ gatsby-node.js
@@ -3,36 +3,3 @@
  *
  * See: https://www.gatsbyjs.com/docs/node-apis/
  */
-const path = require("path")
-const { createFilePath } = require(`gatsby-source-filesystem`)
-
-// exports.onCreateNode = ({ node, getNode, actions }) => {
-//   const { createNodeField } = actions
-
-// }
-
-exports.createPages = async ({ graphql, actions }) => {
-  const { createPage } = actions
-  const result = await graphql(`
-    query {
-      allMarkdownRemark {
-        edges {
-          node {
-            frontmatter {
-              path
-            }
-          }
-        }
-      }
-    }
-  `)
-  result.data.allMarkdownRemark.edges.forEach(({ node }) => {
-    createPage({
-      path: `/${node.frontmatter.path}`,
-      component: path.resolve(`./src/templates/blog-post.js`),
-      context: {
-        slug: node.frontmatter.path
-      }
-    })
-  })
-}
