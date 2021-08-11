e5527ba0cc32f8549fbda2c14ffc7ef2c9cc014d
Trang Le <lethutrang101@gmail.com>
2021-08-11 11:08:33 +0700

    Add blog feed to the home page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,31 +1,42 @@
 import * as React from "react"
-import { Link } from "gatsby"
-import { StaticImage } from "gatsby-plugin-image"
+import { Link, graphql } from "gatsby"
 
 import Layout from "../components/layout"
 import Seo from "../components/seo"
 
-const IndexPage = () => (
+const IndexPage = ({ data }) => (
   <Layout>
     <Seo title="Home" />
     <h1>Hi people</h1>
     <p>Welcome to your new Gatsby site.</p>
     <p>Now go build something great.</p>
     <h2>Blog</h2>
-
-    <StaticImage
-      src="../images/gatsby-astronaut.png"
-      width={300}
-      quality={95}
-      formats={["AUTO", "WEBP", "AVIF"]}
-      alt="A Gatsby astronaut"
-      style={{ marginBottom: `1.45rem` }}
-    />
-    <p>
-      <Link to="/page-2/">Go to page 2</Link> <br />
-      <Link to="/using-typescript/">Go to "Using TypeScript"</Link>
-    </p>
+    {data.allMarkdownRemark.nodes.map(node => (
+      <article key={node.id}>
+          <h2>
+              <Link to={`/blog/${node.frontmatter.path}`}>
+                  {node.frontmatter.title}
+              </Link>
+          </h2>
+          <p>Posted: {node.frontmatter.date}</p>
+      </article>
+    ))}
   </Layout>
 )
+
+export const query = graphql`
+    query {
+        allMarkdownRemark(sort: {fields: frontmatter___date, order: DESC}) {
+            nodes {
+                frontmatter {
+                    date(formatString: "YYYY-MM-DD")
+                    title
+                    path
+                }
+                id
+            }
+        }
+    }
+`
 
 export default IndexPage
