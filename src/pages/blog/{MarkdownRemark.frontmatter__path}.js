4d2c9bc257329b557f27a72ad1e46d6e082e42e4
Trang Le <lethutrang101@gmail.com>
2021-08-11 11:07:06 +0700

    Move blog post template to /blog subdirectory

--- src/pages/blog/{MarkdownRemark.frontmatter__path}.js
+++ src/pages/blog/{MarkdownRemark.frontmatter__path}.js
@@ -1,19 +1,18 @@
-import React from "react"
+import * as React from "react"
 import { graphql } from "gatsby"
+import Layout from '../../components/layout'
 
-export default function Template({
+export default function BlogPost({
     data // this prop will be injected by the graphql query below
 }) {
     const { markdownRemark } = data
     const { frontmatter, html } = markdownRemark
     return (
-        <div className="blog-post-container">
-            <div className="blog-post">
-                <h1>{frontmatter.title}</h1>
-                <h2>{frontmatter.date}</h2>
-                <div className="blog-post-content" dangerouslySetInnerHTML={{__html: html}}/>
-            </div>
-        </div>
+        <Layout pageTitle={frontmatter.title}>
+            <p>{frontmatter.date}</p>
+            <div className="blog-post-content" dangerouslySetInnerHTML={{__html: html}}/>
+        </Layout>
+
     )
 }
 
