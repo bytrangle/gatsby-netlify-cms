20800c00f34afbe283d34c875fc3a57e62d3882d
Trang Le <lethutrang101@gmail.com>
2021-08-21 16:40:41 +0700

    Update Blog page to link to each post

--- src/pages/blog/index.js
+++ src/pages/blog/index.js
@@ -1,5 +1,5 @@
 import * as React from 'react'
-import { graphql } from 'gatsby'
+import { Link, graphql } from 'gatsby'
 import {MDXRenderer} from 'gatsby-plugin-mdx'
 import Layout from '../../components/layout'
 
@@ -9,11 +9,12 @@ const BlogPage = ({ data }) => {
 			{
         data.allMdx.nodes.map((node) => (
           <article key={node.id}>
-            <h2>{node.frontmatter.title}</h2>
+            <h2>
+              <Link to={`/blog/${node.slug}`}>
+                {node.frontmatter.title}
+              </Link>
+            </h2>
             <p>Posted:{node.frontmatter.date}</p>
-            <MDXRenderer>
-                {node.body}
-            </MDXRenderer>
           </article>
         ))
 			}
@@ -24,14 +25,14 @@ const BlogPage = ({ data }) => {
 export const query = graphql`
   query {
     allMdx(sort: {fields: frontmatter___date, order: DESC}) {
-        nodes {
-            frontmatter {
-                date(formatString: "YYYY-MM-DD")
-                title
-            }
-            id
-            body
+      nodes {
+        frontmatter {
+          date(formatString: "YYYY-MM-DD")
+          title
         }
+        id
+        slug
+      }
     }
   }
 `
